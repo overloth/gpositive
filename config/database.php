50779f13aa6a5921687fff9f0a91bd914bcdd4c1
overloth <overloth@yahoo.com>
2017-08-24 18:01:09 +0200

    proba2

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'gpositive'),
+            'username' => env('DB_USERNAME', 'overloth'),
+            'password' => env('DB_PASSWORD', 'lukrecija69'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
