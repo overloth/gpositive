8a3244885bbb32db2f0be387ded8d9c3ac791b74
overloth <overloth@yahoo.com>
2017-09-26 12:23:08 +0200

    redirect after fb login proba

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ use Illuminate\Support\Facades\Log;
 
 //if($_COOKIE['lang']) App::setLocale($_COOKIE['lang']);
 
-Route::get('/', 'HomeController@index') -> name('home');
+Route::get('/', 'HomeController@index');
 Route::get('/language/{locale}', 'HomeController@setLanguage');
 
 Auth::routes();
