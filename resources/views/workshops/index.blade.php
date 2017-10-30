ece42c4759bbecf828aed98d83d2781f796f07b2
overloth <overloth@yahoo.com>
2017-10-30 10:42:56 +0100

    use s3 for upload

--- resources/views/workshops/index.blade.php
+++ resources/views/workshops/index.blade.php
@@ -57,7 +57,7 @@
     <a style="color:black;" href="/workshops/{{$workshop->id}}">
       <div class="w3-col l4 m6 s12 ">
         <div class="w3-card-4 w3-margin w3-white ">
-          <div class="article_image" style="background:url('/{{$workshop->image}}') no-repeat;display:block;margin-left:auto;margin-right:auto;background-size:cover; width:100%; background-position: 0px -50px; height:170px;"> </div>
+          <div class="article_image" style="background:url('{{$workshop->image}}') no-repeat;display:block;margin-left:auto;margin-right:auto;background-size:cover; width:100%; background-position: 0px -50px; height:170px;"> </div>
           <div class="w3-container w3-padding-8">
             <h3 class="article_heading"><b><a href="/workshops/{{$workshop->id}}">{{$workshop->title}}</a></b></h3>
             <p><a href="/authors/{{$workshop->author->id}}">{{$workshop->author->name}}</a><span class="w3-opacity" style="float:right;padding-right:25px;">{{ date('d M Y ', $workshop->created_at->timestamp) }}</span></p>
--- resources/views/workshops/show.blade.php
+++ resources/views/workshops/show.blade.php
@@ -4,7 +4,7 @@
 
   <div class="w3-card-4 w3-margin w3-white ">
 
-    <div class="article_image" style="background:url('/{{$workshop->image}}') no-repeat;display:block;margin-left:auto;margin-right:auto;background-size:cover; width:100%; background-position: 0px -50px; height:500px;"> </div>
+    <div class="article_image" style="background:url('{{$workshop->image}}') no-repeat;display:block;margin-left:auto;margin-right:auto;background-size:cover; width:100%; background-position: 0px -50px; height:500px;"> </div>
     <div class="w3-container w3-padding-8">
        
       <h3 class="article_heading"><b>{{$workshop->title}}</b></h3>
