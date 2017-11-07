a3b6d8a5a681eff9fb1cbf33e76abfd4bf75213e
overloth <overloth@yahoo.com>
2017-11-07 09:25:06 +0100

    razdvajanje i ubacivanje tagova

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -45,23 +45,23 @@ class ArticleController extends Controller
     {
         //not logged in, no editing articles
         if(!Auth::user())
-        {
-            dd('there was problem saying you are not logged in');
-            return;
-        }
+            {
+                dd('there was problem saying you are not logged in');
+                return;
+            }
 
         // check if author logged in
-        if(!Auth::user()->author)
-        {
-            dd('there was problem saying you are not author');
-            return;
-        }
-        
-        $tags = Tag::pluck('name', 'id');
-        $courses = [0 => 'None'] + DB::table('courses')->pluck('title','id')->toArray();
+            if(!Auth::user()->author)
+                {
+                    dd('there was problem saying you are not author');
+                    return;
+                }
 
-        return view('articles.create', compact('courses', 'tags'));
-    }
+                $tags = Tag::pluck('name', 'id');
+                $courses = [0 => 'None'] + DB::table('courses')->pluck('title','id')->toArray();
+
+                return view('articles.create', compact('courses', 'tags'));
+            }
 
     /**
      * Store a newly created resource in storage.
@@ -75,110 +75,115 @@ class ArticleController extends Controller
         //$request->input('author') = 'Gorana Rakic-Bajic';
 
         if(!Auth::user())
-        {
-            dd('there was problem saying you are not logged in');
-            return;
-        }
+            {
+                dd('there was problem saying you are not logged in');
+                return;
+            }
 
-        if(!Auth::user()->author)
-        {
-            dd('there was problem saying you are not author');
-            return;
-        }
+            if(!Auth::user()->author)
+                {
+                    dd('there was problem saying you are not author');
+                    return;
+                }
 
-        $inss = $request->all();
+                $inss = $request->all();
         //dd($inss);
         //$user = Auth::user()->author->id;
-        $inss['author_id'] = Auth::user()->author->id;
+                $inss['author_id'] = Auth::user()->author->id;
         //$inss['course_id'] = 0;
-        $article = Article::create($inss);
+                $article = Article::create($inss);
 
         //dd($request->input('tag_list'));
-        
-        
-        //adding new tag
-        $tag = $inss['new_tag'];
-        $tagdone = Tag::create(['name'=>$tag]);
-        $tagid = $tagdone->id;
-       //dd($inss,$tag, $tagdone, $tagid);
-        $article->tags()->attach($request->input('tag_list'));
-        $article->tags()->attach($tagid);
 
-       
-    
+
+        //adding new tag
+                $tag = explode(' ' , $inss['new_tag']);
+                $countTags=count($tag);
+                //dd($tag, $countTags);
+                for ($i=0; $i < $countTags; $i++) { 
+             # code...
+                    $tagdone = Tag::create(['name'=>$tag[$i]]);
+                    $tagid = $tagdone->id;
+       //dd($inss,$tag, $tagdone, $tagid);
+
+                    $article->tags()->attach($tagid);
+                }
+
+                $article->tags()->attach($request->input('tag_list'));
+
         //$article->author = 'Gorana Rakic-Bajic';
 
-        if ($request->hasFile('image')) {
+                if ($request->hasFile('image')) {
 
-            if ($request->file('image')->isValid()) {
+                    if ($request->file('image')->isValid()) {
 
-                $file = $request->file('image');
-                
+                        $file = $request->file('image');
+
                 //set upload path
                // $destinationPath = 'uploads';
                 //get filename
-                $filename = $request->file('image')->getClientOriginalName();
-                $uniqFilename = md5($filename . time());
-                $extension = \File::extension($filename);
-                $newName = $uniqFilename . '.' . $extension;
+                        $filename = $request->file('image')->getClientOriginalName();
+                        $uniqFilename = md5($filename . time());
+                        $extension = \File::extension($filename);
+                        $newName = $uniqFilename . '.' . $extension;
                 //uploading file to given path
                //Storage::disk('s3')->put('uploads/' . $filename, file_get_contents($file), 'public');
                // $destinationPath = Storage::disk('s3')->url($filename)
                 // set up s3
-                $bucket = getenv('S3_BUCKET');
-                $address = getenv('S3_ADDRESS');
-                $keyname = 'uploads/'.$newName;
-                $s3 = S3Client::factory([
-                    'version' => '2006-03-01',
-                    'region' => 'us-east-2'
-                ]);
-    
+                        $bucket = getenv('S3_BUCKET');
+                        $address = getenv('S3_ADDRESS');
+                        $keyname = 'uploads/'.$newName;
+                        $s3 = S3Client::factory([
+                            'version' => '2006-03-01',
+                            'region' => 'us-east-2'
+                        ]);
+
                 // try
-                
+
                     // Upload data.
-                    $s3->putObject(array(
-                        'Bucket' => $bucket,
-                        'Key'    => $keyname,
-                        'Body'   => fopen($_FILES['image']['tmp_name'], 'rb'),
-                        'ACL'    => 'public-read'
-                    ));
-    
-                
-               
+                        $s3->putObject(array(
+                            'Bucket' => $bucket,
+                            'Key'    => $keyname,
+                            'Body'   => fopen($_FILES['image']['tmp_name'], 'rb'),
+                            'ACL'    => 'public-read'
+                        ));
+
+
+
 
               //  $request->file('image')->move($destinationPath, $filename);
-               
+
 
                 //set item image
-                $article->image = $address . $bucket . '/' . $keyname;
+                        $article->image = $address . $bucket . '/' . $keyname;
                 //save
-               $article->save();
+                        $article->save();
 
-            }
-            else
-            {
+                    }
+                    else
+                    {
                 //there was problem uploading image
-                dd('there was problem uploading image');
-            }
+                        dd('there was problem uploading image');
+                    }
 
-            
-    
-    
-    
-    
-    
-    
-    
-}
-else 
-        {
+
+
+
+
+
+
+
+
+                }
+                else 
+                {
             //image file not uploaded
-            dd('image file not uploaded');
-        }
+                    dd('image file not uploaded');
+                }
 
 
-        return redirect('articles');
-    }
+                return redirect('articles');
+            }
 
     /**
      * Display the specified resource.
@@ -203,32 +208,32 @@ else
     {
         //not logged in, no editing articles
         if(!Auth::user())
-        {
-            dd('there was problem saying you are not logged in');
-            return;
-        }
+            {
+                dd('there was problem saying you are not logged in');
+                return;
+            }
 
         //check if user is author
-        if(!Auth::user()->author && !Auth::user('id'=='11'))
-        {
-            dd('there was problem saying you are not author');
-            return;
-        }
+            if(!Auth::user()->author && !Auth::user('id'=='11'))
+                {
+                    dd('there was problem saying you are not author');
+                    return;
+                }
 
-        $article = Article::findOrFail($id);
+                $article = Article::findOrFail($id);
 
         //check if author is editing his article
-        if(Auth::user()->author->id != $article->author_id && !Auth::user('id'=='11'))
-        {
-            dd('there was problem saying you are not author of this article');
-            return;
-        }
+                if(Auth::user()->author->id != $article->author_id && !Auth::user('id'=='11'))
+                    {
+                        dd('there was problem saying you are not author of this article');
+                        return;
+                    }
 
-        $tags = Tag::pluck('name', 'id')->toArray();
-        $courses = [0 => 'None'] + DB::table('courses')->pluck('title','id')->toArray();
+                    $tags = Tag::pluck('name', 'id')->toArray();
+                    $courses = [0 => 'None'] + DB::table('courses')->pluck('title','id')->toArray();
 
-        return view('articles.edit', compact('article', 'courses', 'tags'));
-    }
+                    return view('articles.edit', compact('article', 'courses', 'tags'));
+                }
 
     /**
      * Update the specified resource in storage.
@@ -241,106 +246,106 @@ else
     {
         //not logged in, no updating articles
         if(!Auth::user())
-        {
-            dd('there was problem saying you are not logged in');
-            return;
-        }
+            {
+                dd('there was problem saying you are not logged in');
+                return;
+            }
 
         //check if user is author
-        if(!Auth::user()->author)
-        {
-            dd('there was problem saying you are not author');
-            return;
-        }
+            if(!Auth::user()->author)
+                {
+                    dd('there was problem saying you are not author');
+                    return;
+                }
 
         //find specified article
-        $article = Article::findOrFail($id);
+                $article = Article::findOrFail($id);
 
         //check if author is editing his article
-        if(Auth::user()->author->id != $article->author_id && !Auth::user('id'=='11'))
-        {
-            dd('there was problem saying you are not author of this article');
-            return;
-        }
-        
-        
+                if(Auth::user()->author->id != $article->author_id && !Auth::user('id'=='11'))
+                    {
+                        dd('there was problem saying you are not author of this article');
+                        return;
+                    }
+
+
 
         // and update it
-        $article->update($request->all());
+                    $article->update($request->all());
 
-        $article->tags()->sync($request->input('tag_list'));
+                    $article->tags()->sync($request->input('tag_list'));
 
-         if ($request->hasFile('image')) {
+                    if ($request->hasFile('image')) {
 
-            if ($request->file('image')->isValid()) {
+                        if ($request->file('image')->isValid()) {
 
-                $file = $request->file('image');
-                
+                            $file = $request->file('image');
+
                 //set upload path
                // $destinationPath = 'uploads';
                 //get filename
-                $filename = $request->file('image')->getClientOriginalName();
-                $uniqFilename = md5($filename . time());
-                $extension = \File::extension($filename);
-                $newName = $uniqFilename . '.' . $extension;
+                            $filename = $request->file('image')->getClientOriginalName();
+                            $uniqFilename = md5($filename . time());
+                            $extension = \File::extension($filename);
+                            $newName = $uniqFilename . '.' . $extension;
                 //uploading file to given path
                //Storage::disk('s3')->put('uploads/' . $filename, file_get_contents($file), 'public');
                // $destinationPath = Storage::disk('s3')->url($filename)
                 // set up s3
-                $bucket = getenv('S3_BUCKET');
-                $address = getenv('S3_ADDRESS');
-                $keyname = 'uploads/'.$newName;
-                $s3 = S3Client::factory([
-                    'version' => '2006-03-01',
-                    'region' => 'us-east-2'
-                ]);
-    
+                            $bucket = getenv('S3_BUCKET');
+                            $address = getenv('S3_ADDRESS');
+                            $keyname = 'uploads/'.$newName;
+                            $s3 = S3Client::factory([
+                                'version' => '2006-03-01',
+                                'region' => 'us-east-2'
+                            ]);
+
                 // try
-                
+
                     // Upload data.
-                    $s3->putObject(array(
-                        'Bucket' => $bucket,
-                        'Key'    => $keyname,
-                        'Body'   => fopen($_FILES['image']['tmp_name'], 'rb'),
-                        'ACL'    => 'public-read'
-                    ));
-    
-                
-               
+                            $s3->putObject(array(
+                                'Bucket' => $bucket,
+                                'Key'    => $keyname,
+                                'Body'   => fopen($_FILES['image']['tmp_name'], 'rb'),
+                                'ACL'    => 'public-read'
+                            ));
+
+
+
 
               //  $request->file('image')->move($destinationPath, $filename);
-               
+
 
                 //set item image
-                $article->image = $address . $bucket . '/' . $keyname;
+                            $article->image = $address . $bucket . '/' . $keyname;
                 //save
-               $article->save();
+                            $article->save();
 
-            }
-            else
-            {
+                        }
+                        else
+                        {
                 //there was problem uploading image
-                dd('there was problem uploading image');
-            }
+                            dd('there was problem uploading image');
+                        }
 
-            
-    
-    
-    
-    
-    
-    
-    
-}
-else 
-        {
+
+
+
+
+
+
+
+
+                    }
+                    else 
+                    {
             //image file not uploaded
-            dd('image file not uploaded');
-        }
+                        dd('image file not uploaded');
+                    }
 
 
-        return redirect('articles');
-    }
+                    return redirect('articles');
+                }
 
     /**
      * Remove the specified resource from storage.
@@ -352,30 +357,30 @@ else
     {
         //not logged in, no updating articles
         if(!Auth::user())
-        {
-            dd('there was problem saying you are not logged in');
-            return;
-        }
+            {
+                dd('there was problem saying you are not logged in');
+                return;
+            }
 
         //check if user is author
-        if(!Auth::user()->author )
-        {
-            dd('there was problem saying you are not author');
-            return;
-        }
+            if(!Auth::user()->author )
+                {
+                    dd('there was problem saying you are not author');
+                    return;
+                }
 
         //find specified article
-        $article = Article::findOrFail($id);
+                $article = Article::findOrFail($id);
 
         //check if author is editing his article
-        if(Auth::user()->author->id != $article->author_id && !Auth::user('id'=='11'))
-        {
-            dd('there was problem saying you are not author of this article');
-            return;
-        }
+                if(Auth::user()->author->id != $article->author_id && !Auth::user('id'=='11'))
+                    {
+                        dd('there was problem saying you are not author of this article');
+                        return;
+                    }
 
-        $article->delete();
-        
-        return redirect('articles');
-    }
-}
+                    $article->delete();
+
+                    return redirect('articles');
+                }
+            }
